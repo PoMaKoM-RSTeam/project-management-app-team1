6523aa49c6aebc8d0801c08628e82e639d4d16e0
zhagun <zhagunpv@gmail.com>
2022-11-04 21:49:01 +0300

    feat: add project-create modal

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,3 +1,6 @@
+import { ProjectCreateUpdateModalComponent } from './../../../shared/components/project-create-update-modal/project-create-update-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ICreateEditProject } from './../../models/dialog.model';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -10,10 +13,32 @@ import { TranslateService } from '@ngx-translate/core';
 export class HeaderComponent {
   private currentLang = 'en';
 
-  constructor(public translate: TranslateService) {}
+  constructor(public translate: TranslateService,
+    private projectModal: MatDialog) {}
 
   switchLang() {
     this.currentLang = this.currentLang === 'en' ? 'ru' : 'en';
     this.translate.use(this.currentLang);
+  }
+
+  createProject() {
+    const dialogData: ICreateEditProject = {
+      title:'Project-modal-add-title',
+      projectTitleLabel:'Project-modal-title',
+      projectDescriptionLabel:'Project-modal-description',
+      commandName:'Project-modal-add'
+    };
+
+
+    const dialogRef = this.projectModal.open(ProjectCreateUpdateModalComponent, {
+      maxWidth: '600px',
+      data: dialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((dialogResult) => {
+      if (dialogResult) {
+        
+      }
+    });
   }
 }
